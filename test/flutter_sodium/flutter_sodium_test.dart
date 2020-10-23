22bd7428e01a3f3ac57e517976bd08fd872ff913
jaan-c <jaan-c@users.noreply.github.com>
2020-10-23 16:56:12 +0800

    Added test confirming cryptoSecretstreamXchacha20poly1305Keybytes is 256 bits.

--- test/flutter_sodium/flutter_sodium_test.dart
+++ test/flutter_sodium/flutter_sodium_test.dart
@@ -5,4 +5,8 @@ void main() {
   test("Version is 1.0.18", () {
     expect(Sodium.versionString, "1.0.18");
   });
+
+  test("cryptoSecretstreamXchacha20poly1305Keybytes is 256 bits", () {
+    expect(Sodium.cryptoSecretstreamXchacha20poly1305Keybytes, 32);
+  });
 }
