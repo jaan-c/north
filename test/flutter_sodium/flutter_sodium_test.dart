1c28dc7f24ad5394d82a036a915ebd365c118586
jaan-c <jaan-c@users.noreply.github.com>
2020-10-27 14:56:57 +0800

    Initialize flutter_sodium first before all tests.

--- test/flutter_sodium/flutter_sodium_test.dart
+++ test/flutter_sodium/flutter_sodium_test.dart
@@ -2,6 +2,8 @@ import 'package:flutter_sodium/flutter_sodium.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  setUpAll(Sodium.init);
+
   test("Version is 1.0.18", () {
     expect(Sodium.versionString, "1.0.18");
   });
