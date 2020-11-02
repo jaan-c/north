b5df2f2bcad2b40db288a057f6754290ef23d4c0
jaan-c <jaan-c@users.noreply.github.com>
2020-11-02 19:44:21 +0800

    MediaStore initial implementation.

--- lib/src/private_gallery/media_store/media_store.dart
+++ lib/src/private_gallery/media_store/media_store.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:ext_storage/ext_storage.dart';
-import 'package:file/chroot.dart';
-import 'package:file/file.dart';
-import 'package:file/local.dart';
 import 'package:flutter/foundation.dart';
 import 'package:north/crypto.dart';
 import 'package:path/path.dart' as pathlib;
@@ -20,64 +18,99 @@ class MediaStoreException implements Exception {
 
 class MediaStore {
   static const _mediaDirectoryName = '.north';
+  static const _mediaCacheDirectoryName = 'media_cache';
 
-  final Future<FileSystem> _futureFileSystem;
   final String _password;
+  final Future<Directory> _futureMediaDir;
+  final Future<Directory> _futureCacheDir;
 
-  MediaStore._internal(this._password, this._futureFileSystem);
+  MediaStore._internal(
+      this._password, this._futureMediaDir, this._futureCacheDir);
 
-  factory MediaStore({@required String password, FileSystem fileSystem}) {
-    if (fileSystem != null) {
-      return MediaStore._internal(password, Future.value(fileSystem));
+  factory MediaStore(
+      {@required String password,
+      Directory externalRoot,
+      Directory cacheRoot}) {
+    final cacheDir =
+        createCacheDir(_mediaCacheDirectoryName, cacheRoot: cacheRoot);
+
+    if (externalRoot != null) {
+      return MediaStore._internal(
+          password, Future.value(externalRoot), cacheDir);
     }
 
     return MediaStore._internal(password, (() async {
-      final encryptedRoot = pathlib.join(
-          await ExtStorage.getExternalStorageDirectory(), _mediaDirectoryName);
-      return ChrootFileSystem(LocalFileSystem(), encryptedRoot);
-    })());
+      externalRoot ??=
+          Directory(await ExtStorage.getExternalStorageDirectory());
+      return Directory(pathlib.join(externalRoot.path, _mediaDirectoryName))
+          .create();
+    })(), cacheDir);
   }
 
-  Future<Uint8List> put(Uuid id, Stream<Uint8List> content) async {
-    final fileSystem = await _futureFileSystem;
-    final file = fileSystem.file(id.toString());
+  Future<Uint8List> put(Uuid id, File file) async {
+    final mediaDir = await _futureMediaDir;
+    final inFile = file;
+    final outFile = mediaDir.file(id.toString());
 
-    if (await file.exists()) {
+    if (await outFile.exists()) {
       throw MediaStoreException('Media $id already exists.');
     }
 
-    final sink = file.openWrite();
+    final outSink = outFile.openWrite();
     final salt = generateSalt();
+    final plainStream = inFile.openRead();
+    final cipherStream = encryptStream(_password, salt, plainStream.cast());
     try {
-      final cipherStream = encryptStream(_password, salt, content);
-      await sink.addStream(cipherStream);
-      await sink.flush(); // Call only if addStream completes.
+      await outSink.addStream(cipherStream);
+      await outSink.flush(); // Call only if addStream completes.
     } finally {
-      await sink.close();
+      await outSink.close();
     }
 
     return salt;
   }
 
-  Stream<Uint8List> get(Uuid id, Uint8List salt) async* {
-    final fileSystem = await _futureFileSystem;
-    final file = fileSystem.file(id.toString());
+  Future<File> get(Uuid id, Uint8List salt) async {
+    final mediaDir = await _futureMediaDir;
+    final cacheDir = await _futureCacheDir;
 
-    if (!await file.exists()) {
+    final cipherFile = mediaDir.file(id.toString());
+    final cacheFile = await cacheDir.file(id.toString());
+
+    if (!await cipherFile.exists()) {
       throw MediaStoreException('Media $id does not exist.');
     }
 
-    final cipherStream = file.openRead();
-    yield* decryptStream(_password, salt, cipherStream);
+    if (await cacheFile.exists()) {
+      return cacheFile;
+    }
+
+    final cipherStream = cipherFile.openRead();
+    final plainStream = decryptStream(_password, salt, cipherStream.cast());
+    final cacheSink = cacheFile.openWrite();
+    try {
+      await cacheSink.addStream(plainStream);
+      await cacheSink.flush();
+    } finally {
+      await cacheSink.close();
+    }
+
+    return cacheFile;
   }
 
   Future<void> delete(Uuid id) async {
-    final fileSystem = await _futureFileSystem;
+    final mediaDir = await _futureMediaDir;
+    final cacheDir = await _futureCacheDir;
 
-    try {
-      await fileSystem.file(id.toString()).delete();
-    } on FileSystemException catch (e) {
-      throw MediaStoreException('Failed to delete media $id: ${e.message}');
-    }
+    final cipherFile = mediaDir.file(id.toString());
+    final cacheFile = await cacheDir.file(id.toString());
+
+    await cipherFile.delete();
+    await cacheFile.delete();
+  }
+
+  Future<void> clearCache() async {
+    final cacheDir = await _futureCacheDir;
+    await cacheDir.delete(recursive: true);
   }
 }
--- test/private_gallery/media_store/media_store_test.dart
+++ test/private_gallery/media_store/media_store_test.dart
@@ -1,5 +1,62 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:north/src/private_gallery/commons/uuid.dart';
+import 'package:north/src/private_gallery/media_store/media_store.dart';
+
+import '../../utils.dart';
 
 void main() {
-  group('MediaStore', () {});
+  final throwsMediaStoreException =
+      throwsA(isInstanceOf<MediaStoreException>());
+
+  Directory tempDir;
+  Directory mediaDir;
+  Directory cacheDir;
+  MediaStore store;
+
+  setUp(() async {
+    mediaDir = await createTempDir();
+    cacheDir = await createTempDir();
+    tempDir = await createTempDir();
+    store = MediaStore(
+        password: 'Password', externalRoot: mediaDir, cacheRoot: cacheDir);
+  });
+
+  tearDown(() async {
+    await tempDir.delete(recursive: true);
+    await mediaDir.delete(recursive: true);
+    await cacheDir.delete(recursive: true);
+  });
+
+  test('put throws MediaStoreException on existing id.', () async {
+    final id = Uuid.generate();
+    final content = randomBytes(2048);
+    final file = tempDir.file();
+
+    await file.writeAsBytes(content);
+    await store.put(id, file);
+
+    await expectLater(store.put(id, file), throwsMediaStoreException);
+  });
+
+  test('get throws MediaStoreException on non-existent id.', () async {
+    final id = Uuid.generate();
+    final salt = randomBytes(16);
+
+    await expectLater(store.get(id, salt), throwsMediaStoreException);
+  });
+
+  test('get retrieves inserted file with put.', () async {
+    final id = Uuid.generate();
+    final content = randomBytes(2048);
+    final file = tempDir.file();
+    await file.writeAsBytes(content);
+
+    final salt = await store.put(id, file);
+    final retrievedFile = await store.get(id, salt);
+    final retrievedContent = await retrievedFile.readAsBytes();
+
+    expect(content, retrievedContent);
+  });
 }
