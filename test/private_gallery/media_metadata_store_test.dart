614d77d118704d54e0ac1d198be31d6b3918cc62
jaan-c <jaan-c@users.noreply.github.com>
2020-11-03 16:45:48 +0800

    Fix wrong delete in 41988200e1d0429498b4158a11c70f26f567e811.

---
+++ test/private_gallery/media_metadata_store_test.dart
@@ -0,0 +1,82 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:north/private_gallery.dart';
+
+import '../utils.dart';
+
+void main() {
+  group('MediaMetadata', () {
+    test('== implements proper structural equality.', () {
+      final salt = randomBytes(16);
+      final m1 = MediaMetadata(
+          album: 'Test Album',
+          name: 'Test Media',
+          salt: salt,
+          storeDateTime: DateTime(2020, 12, 25),
+          type: MediaType.image);
+      final m2 = MediaMetadata(
+          album: 'Test Album',
+          name: 'Test Media',
+          salt: salt,
+          storeDateTime: DateTime(2020, 12, 25),
+          type: MediaType.image);
+      final m3 = MediaMetadata(
+          album: 'Test Album',
+          name: 'Test Media',
+          salt: randomBytes(16),
+          storeDateTime: DateTime(2020, 12, 25),
+          type: MediaType.image);
+
+      expect(m1, m2);
+      expect(m1, isNot(equals(m3)));
+    });
+  });
+
+  group('MediaMetadataStore', () {
+    MediaMetadataStore store;
+    MediaMetadata metadata;
+
+    setUp(() {
+      store = MediaMetadataStore(shouldPersist: false);
+      metadata = MediaMetadata(
+          album: 'Test Album',
+          name: 'Test Media',
+          salt: randomBytes(16),
+          storeDateTime: DateTime(2020, 12, 25),
+          type: MediaType.image);
+    });
+
+    tearDown(() async {
+      store = null;
+      metadata = null;
+    });
+
+    test('put throws on existing id.', () async {
+      final id = Uuid.generate();
+
+      await expectLater(store.put(id, metadata), completes);
+      await expectLater(store.put(id, metadata),
+          throwsA(isInstanceOf<MediaMetadataStoreException>()));
+    });
+
+    test('get throws on non-existing id.', () async {
+      final id = Uuid.generate();
+      final nonExistingId = Uuid.generate();
+
+      await store.put(id, metadata);
+
+      await expectLater(store.get(id), completion(metadata));
+      await expectLater(store.get(nonExistingId),
+          throwsA(isInstanceOf<MediaMetadataStoreException>()));
+    });
+
+    test('delete throws on non-existing id.', () async {
+      final id = Uuid.generate();
+
+      await store.put(id, metadata);
+
+      await expectLater(store.delete(id), completes);
+      await expectLater(store.delete(id),
+          throwsA(isInstanceOf<MediaMetadataStoreException>()));
+    });
+  });
+}
