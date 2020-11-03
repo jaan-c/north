0dfb475d16a003d277e30a789604b44e84b88605
jaan-c <jaan-c@users.noreply.github.com>
2020-11-03 09:23:05 +0800

    Completely flatten src/private_gallery.

--- lib/private_gallery.dart
+++ lib/private_gallery.dart
@@ -1,4 +1,5 @@
 export 'src/private_gallery/uuid.dart';
+export 'src/private_gallery/media_metadata.dart';
 export 'src/private_gallery/media_metadata_store.dart';
 export 'src/private_gallery/media_store.dart';
 export 'src/private_gallery/thumbnail_store.dart';
--- lib/src/private_gallery/media_metadata_store.dart
+++ lib/src/private_gallery/media_metadata_store.dart
@@ -1,2 +1,77 @@
-export 'media_metadata_store/media_metadata_store.dart';
-export 'media_metadata_store/media_metadata.dart';
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'media_metadata.dart';
+import 'uuid.dart';
+
+class MediaMetadataStoreException implements Exception {
+  final String message;
+  MediaMetadataStoreException(this.message);
+  @override
+  String toString() => 'MediaMetadataStorageException: $message';
+}
+
+class MediaMetadataStore {
+  static const _boxName = 'media_metadata';
+
+  final Future<Box<MediaMetadata>> _futureBox;
+
+  MediaMetadataStore({bool shouldPersist = true})
+      : _futureBox = (() async {
+          await _HiveInitializer.init();
+          return await Hive.openBox<MediaMetadata>(_boxName,
+              bytes: shouldPersist ? null : Uint8List.fromList([]));
+        })();
+
+  Future<void> put(Uuid id, MediaMetadata metadata) async {
+    final box = await _futureBox;
+
+    if (box.containsKey(id.toString())) {
+      throw MediaMetadataStoreException('$id id already exists.');
+    }
+
+    await box.put(id.toString(), metadata);
+  }
+
+  Future<MediaMetadata> get(Uuid id) async {
+    final box = await _futureBox;
+
+    if (!box.containsKey(id.toString())) {
+      throw MediaMetadataStoreException('$id id does not exist.');
+    }
+
+    return box.get(id.toString());
+  }
+
+  Future<void> delete(Uuid id) async {
+    final box = await _futureBox;
+
+    if (!box.containsKey(id.toString())) {
+      throw MediaMetadataStoreException('$id id does not exist.');
+    }
+
+    await box.delete(id.toString());
+  }
+
+  Future<void> close() async {
+    final box = await _futureBox;
+    await box.close();
+  }
+}
+
+class _HiveInitializer {
+  static var _isInitialized = false;
+
+  static Future<void> init() async {
+    if (!_isInitialized) {
+      await Hive.initFlutter();
+      Hive.registerAdapter(MediaTypeAdapter());
+      Hive.registerAdapter(MediaMetadataAdapter());
+
+      _isInitialized = true;
+    }
+  }
+}
