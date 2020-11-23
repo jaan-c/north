5291b405949b41234b454d6772e58497f934fdcc
jaan-c <jaan-c@users.noreply.github.com>
2020-11-24 01:54:18 +0800

    Added SharedPreferences class for accessing predefined preferences.

---
+++ lib/src/shared_preferences.dart
@@ -0,0 +1,25 @@
+import 'package:shared_preferences/shared_preferences.dart' as sp;
+
+class SharedPreferences {
+  static const _passwordHashKey = 'passwordHashKey';
+
+  static SharedPreferences _instance;
+
+  static SharedPreferences getInstance() {
+    _instance ??= SharedPreferences._internal();
+
+    return _instance;
+  }
+
+  SharedPreferences._internal();
+
+  Future<String> getPasswordHash() async {
+    final prefs = await sp.SharedPreferences.getInstance();
+    return prefs.getString(_passwordHashKey) ?? '';
+  }
+
+  Future<void> setPasswordHash(String hash) async {
+    final prefs = await sp.SharedPreferences.getInstance();
+    return prefs.setString(_passwordHashKey, hash);
+  }
+}
