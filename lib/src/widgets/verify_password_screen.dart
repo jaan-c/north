22588ff54527c14b2b15b22a60131ad72288c249
jaan-c <jaan-c@users.noreply.github.com>
2020-11-24 02:09:39 +0800

    Decouple PasswordScreen implementation from VerifyPasswordScreen.
    This is so PasswordScreen can be reused for setting password too.

--- lib/src/widgets/verify_password_screen.dart
+++ lib/src/widgets/verify_password_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:north/shared_preferences.dart';
 import 'package:north/src/crypto/password.dart';
 
+import 'password_screen.dart';
+
 typedef SubmitPasswordCallback = void Function(String password);
 
 class VerifyPasswordScreen extends StatefulWidget {
@@ -21,77 +23,16 @@ class _VerifyPasswordScreenState extends State<VerifyPasswordScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: _body(context)),
-      floatingActionButton: _submitFab(context),
-    );
+    return PasswordScreen(
+        onSubmitPassword: _onSubmitPassword, onCheckPassword: _onCheckPassword);
   }
 
-  Widget _body(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-
-    return Center(
-      child: Padding(
-        child: Column(
-          children: [
-            Text('Enter Password', style: textTheme.headline5),
-            SizedBox(height: 16),
-            _passwordField(context),
-          ],
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-        ),
-        padding: const EdgeInsets.all(16),
-      ),
-    );
+  void _onSubmitPassword(String password) {
+    widget.onSubmitPassword(password);
   }
 
-  Widget _passwordField(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-
-    return TextField(
-      controller: passwordController,
-      style: textTheme.subtitle1,
-      decoration: InputDecoration(border: OutlineInputBorder()),
-      obscureText: true,
-      autofocus: true,
-      autocorrect: false,
-    );
-  }
-
-  Widget _submitFab(BuildContext context) {
-    return FloatingActionButton.extended(
-      icon: Icon(Icons.check),
-      label: Text('Submit'),
-      onPressed: isCheckingPassword ? null : () => _onSubmitPassword(context),
-      disabledElevation: 0,
-    );
-  }
-
-  void _onSubmitPassword(BuildContext context) async {
-    final password = passwordController.text;
-
-    if (password.isEmpty) {
-      _showSnackBar(context, 'Empty password.');
-      return;
-    }
-
-    _showSnackBar(context, 'Checking password.');
-    isCheckingPassword = true;
-
-    if (verifyPasswordWithHash(password, await prefs.getPasswordHash())) {
-      widget.onSubmitPassword(password);
-    } else {
-      _showSnackBar(context, 'Wrong password.');
-    }
-
-    isCheckingPassword = false;
-  }
-
-  void _showSnackBar(BuildContext context, String message) {
-    final messenger = ScaffoldMessenger.of(context);
-    final snackBar =
-        SnackBar(content: Text(message), duration: Duration(seconds: 1));
-    messenger.showSnackBar(snackBar);
+  Future<bool> _onCheckPassword(String password) async {
+    final hash = await prefs.getPasswordHash();
+    return verifyPasswordWithHash(password, hash);
   }
 }
