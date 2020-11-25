c15db0fe553e2e8d4d950603703f5ef38e48bf1d
jaan-c <jaan-c@users.noreply.github.com>
2020-11-25 20:30:20 +0800

    Extract generalized password section implementation.

---
+++ lib/src/widgets/password_section.dart
@@ -0,0 +1,115 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+typedef SubmitPasswordCallback = FutureOr<void> Function(String password);
+typedef CheckPasswordCallback = FutureOr<bool> Function(String password);
+
+/// A section for accepting a password from user.
+class PasswordSection extends StatefulWidget {
+  final String title;
+  final SubmitPasswordCallback onSubmitPassword;
+  final CheckPasswordCallback onCheckPassword;
+
+  PasswordSection(
+      {@required this.title,
+      @required this.onSubmitPassword,
+      this.onCheckPassword});
+
+  @override
+  _PasswordSectionState createState() => _PasswordSectionState();
+}
+
+class _PasswordSectionState extends State<PasswordSection> {
+  final passwordController = TextEditingController();
+  var obscurePassword = true;
+  var isCheckingPassword = false;
+
+  bool get isPasswordValid => passwordController.text.isNotEmpty;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        child: Column(
+          children: [
+            _titleText(context),
+            if (isCheckingPassword) ...[
+              LinearProgressIndicator(),
+              _checkingPasswordText(context),
+            ] else ...[
+              _passwordField(context),
+              _submitButton(context),
+            ],
+          ],
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 16),
+      ),
+    );
+  }
+
+  Widget _titleText(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Text(widget.title, style: textTheme.headline5);
+  }
+
+  Widget _passwordField(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return TextField(
+      controller: passwordController,
+      style: textTheme.subtitle1,
+      decoration: InputDecoration(
+        suffixIcon: IconButton(
+          icon: Icon(obscurePassword
+              ? Icons.visibility_rounded
+              : Icons.visibility_off_rounded),
+          onPressed: _toggleObscurePassword,
+        ),
+        border: OutlineInputBorder(),
+      ),
+      obscureText: obscurePassword,
+      autofocus: true,
+      autocorrect: false,
+    );
+  }
+
+  Widget _submitButton(BuildContext context) {
+    return ElevatedButton(
+      child: Text('Submit'),
+      onPressed: isPasswordValid ? _submitPassword : null,
+      autofocus: false,
+      clipBehavior: Clip.antiAlias,
+    );
+  }
+
+  Widget _checkingPasswordText(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Text('Checking Password', style: textTheme.subtitle2);
+  }
+
+  Future<void> _submitPassword() async {
+    final password = passwordController.text;
+    if (await _checkPassword(password)) {
+      await widget.onSubmitPassword(password);
+    }
+  }
+
+  Future<bool> _checkPassword(String password) async {
+    if (widget.onCheckPassword == null) {
+      return true;
+    }
+
+    setState(() => isCheckingPassword = true);
+    final result = await widget.onCheckPassword(password);
+    setState(() => isCheckingPassword = false);
+
+    return result;
+  }
+
+  void _toggleObscurePassword() {
+    setState(() => obscurePassword = !obscurePassword);
+  }
+}
--- lib/src/widgets/set_password_section.dart
+++ lib/src/widgets/set_password_section.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:north/app_preferences.dart';
 import 'package:north/crypto.dart';
 
+import 'password_section.dart';
+
 /// A section for accepting a password from user and setting password hash in
 /// [AppPreferences] unconditionally.
 class SetPasswordSection extends StatefulWidget {
@@ -11,68 +13,17 @@ class SetPasswordSection extends StatefulWidget {
 
 class _SetPasswordSectionState extends State<SetPasswordSection> {
   final prefs = AppPreferences.getInstance();
-  final passwordController = TextEditingController();
-  var obscurePassword = true;
-
-  bool get isPasswordValid => passwordController.text.isNotEmpty;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          _titleText(context),
-          _passwordField(context),
-          _submitButton(),
-        ],
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.center,
-      ),
+    return PasswordSection(
+      title: 'Set Password',
+      onSubmitPassword: _setPassword,
     );
   }
 
-  Widget _titleText(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-    return Text('Set Password', style: textTheme.headline5);
-  }
-
-  Widget _passwordField(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-
-    return TextField(
-      controller: passwordController,
-      style: textTheme.subtitle1,
-      decoration: InputDecoration(
-        suffixIcon: IconButton(
-          icon: Icon(obscurePassword
-              ? Icons.visibility_rounded
-              : Icons.visibility_off_rounded),
-          onPressed: _toggleObscurePassword,
-        ),
-        border: OutlineInputBorder(),
-      ),
-      obscureText: obscurePassword,
-      autofocus: true,
-      autocorrect: false,
-    );
-  }
-
-  Widget _submitButton() {
-    return ElevatedButton(
-      child: Text('Submit'),
-      onPressed: isPasswordValid ? _setPassword : null,
-      autofocus: false,
-      clipBehavior: Clip.antiAlias,
-    );
-  }
-
-  Future<void> _setPassword() async {
-    final password = passwordController.text;
+  Future<void> _setPassword(String password) async {
     final hash = await derivePasswordHash(password);
     await prefs.setPasswordHash(hash);
-  }
-
-  void _toggleObscurePassword() {
-    setState(() => obscurePassword = !obscurePassword);
   }
 }
