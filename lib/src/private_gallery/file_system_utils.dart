e9d5380948848a2df02312c9c2c1f92f91c64a30
jaan-c <jaan-c@users.noreply.github.com>
2020-11-30 16:02:23 +0800

    Remove unused function.

--- lib/src/private_gallery/file_system_utils.dart
+++ lib/src/private_gallery/file_system_utils.dart
@@ -1,14 +1,6 @@
 import 'dart:io';
 
 import 'package:path/path.dart' as pathlib;
-import 'package:path_provider/path_provider.dart';
-
-Future<Directory> createCacheDir(String name, {Directory cacheRoot}) async {
-  cacheRoot ??= (await getExternalCacheDirectories())
-      .firstWhere((dir) => pathlib.isWithin('/storage/emulated', dir.path));
-
-  return cacheRoot.directory(name).create();
-}
 
 extension FileSystemEntityWithin on Directory {
   /// Return a [File] with [name] inside [Directory].
