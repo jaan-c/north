f09f8636ac656d84044c93187beff3433d03ad23
jaan-c <jaan-c@users.noreply.github.com>
2020-12-01 20:10:42 +0800

    Expose cancelable_future.dart since it's a part of PrivateGallery API.

--- lib/private_gallery.dart
+++ lib/private_gallery.dart
@@ -1,3 +1,4 @@
 export 'src/private_gallery/private_gallery.dart';
 export 'src/private_gallery/uuid.dart';
 export 'src/private_gallery/file_store.dart' show FileStoreException;
+export 'src/private_gallery/cancelable_future.dart';
