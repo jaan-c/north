a6b87aeae1532979343559967acb8720689c5d83
jaan-c <jaan-c@users.noreply.github.com>
2020-12-04 22:37:24 +0800

    Don't forget to set textBaseline!
    Another fun Flutter gotcha!

--- lib/src/private_gallery_screen/thumbnail_grid.dart
+++ lib/src/private_gallery_screen/thumbnail_grid.dart
@@ -112,6 +112,7 @@ class ThumbnailTileState extends State<ThumbnailTile> {
       ],
       mainAxisSize: MainAxisSize.max,
       crossAxisAlignment: CrossAxisAlignment.baseline,
+      textBaseline: TextBaseline.alphabetic,
     );
   }
 }
