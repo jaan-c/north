8fd286966dd794005255d5ea96e955fff9b866c8
jaan-c <jaan-c@users.noreply.github.com>
2020-12-10 23:31:39 +0800

    Decouple ThumbnailGrid and ThumbnailTile.

--- lib/src/private_gallery_page/thumbnail_tile.dart
+++ lib/src/private_gallery_page/thumbnail_tile.dart
@@ -1,38 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-
-class ThumbnailGrid extends StatelessWidget {
-  final List<Widget> children;
-  final EdgeInsetsGeometry padding;
-  final int crossAxisCount;
-  final double mainAxisSpacing;
-  final double crossAxisSpacing;
-
-  ThumbnailGrid(
-      {this.children = const [],
-      this.padding = const EdgeInsets.all(0),
-      this.crossAxisCount = 2,
-      this.mainAxisSpacing = 0,
-      this.crossAxisSpacing = 0});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      child: StaggeredGridView.countBuilder(
-        itemBuilder: (_, ix) => children[ix],
-        staggeredTileBuilder: (_) => StaggeredTile.fit(1),
-        itemCount: children.length,
-        crossAxisCount: crossAxisCount,
-        mainAxisSpacing: mainAxisSpacing,
-        crossAxisSpacing: crossAxisSpacing,
-        shrinkWrap: true,
-      ),
-      padding: padding,
-    );
-  }
-}
 
 enum ThumbnailTileMode { normal, unselected, selected }
 
@@ -137,6 +105,7 @@ class _ThumbnailTileState extends State<ThumbnailTile> {
           child: snapshot.hasData
               ? Image.file(snapshot.data)
               : Container(color: Colors.grey),
+          fit: BoxFit.cover,
         );
       },
     );
