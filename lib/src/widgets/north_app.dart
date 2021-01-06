63bf41435879a683055fbcac0a8429a1aff4425e
jaan-c <jaan-c@users.noreply.github.com>
2021-01-06 23:25:20 +0800

    Refactor NorthApp to use the new AuthenticationModel and GalleryModel.

--- lib/src/widgets/north_app.dart
+++ lib/src/widgets/north_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'album_listing_page.dart';
+import 'authentication_model.dart';
 import 'authentication_page.dart';
 import 'gallery_model.dart';
 import 'media_listing_page.dart';
@@ -17,25 +18,29 @@ class _NorthAppState extends State<NorthApp> {
   @override
   Widget build(BuildContext context) {
     return ModelsProvider(
-      builder: (_, __) => _app(context),
+      builder: (context, __) => _app(context),
     );
   }
 
   Widget _app(BuildContext context) {
+    final auth = context.watch<AuthenticationModel>();
+
+    if (auth.status != AuthenticationStatus.open) {
+      return MaterialApp(
+        home: AuthenticationPage(),
+      );
+    }
+
     final gallery = context.watch<GalleryModel>();
 
     return MaterialApp(
       home: Navigator(
         pages: [
-          if (!gallery.isOpen)
-            MaterialPage(child: AuthenticationPage())
-          else ...[
-            MaterialPage(child: AlbumListingPage()),
-            if (gallery.openedAlbum.isNotEmpty)
-              MaterialPage(child: MediaListingPage()),
-            if (gallery.openedMedia != null)
-              MaterialPage(child: MediaViewerPage()),
-          ],
+          MaterialPage(child: AlbumListingPage()),
+          if (gallery.openedAlbum.isNotEmpty)
+            MaterialPage(child: MediaListingPage()),
+          if (gallery.openedMedia != null)
+            MaterialPage(child: MediaViewerPage()),
         ],
         onPopPage: (route, result) => route.didPop(result),
       ),
