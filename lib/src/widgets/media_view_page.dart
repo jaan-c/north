892362737e827e1d44fa1215063a5be993297e1f
jaan-c <jaan-c@users.noreply.github.com>
2021-01-07 20:10:14 +0800

    Refactor MediaViewPage to use the new GalleryModel.

--- lib/src/widgets/media_view_page.dart
+++ lib/src/widgets/media_view_page.dart
@@ -1,98 +1,127 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:north/private_gallery.dart';
+import 'package:mime/mime.dart';
+import 'package:provider/provider.dart';
 
-import 'image_viewer_page.dart';
-import 'video_viewer_page.dart';
+import 'gallery_model.dart';
+import 'media_view.dart';
 
-class MediaViewerPage extends StatefulWidget {
+class MediaViewPage extends StatefulWidget {
   @override
-  _MediaViewerPageState createState() => _MediaViewerPageState();
+  _MediaViewPageState createState() => _MediaViewPageState();
 }
 
-class _MediaViewerPageState extends State<MediaViewerPage> {
+class _MediaViewPageState extends State<MediaViewPage> {
+  Future<File> futureMediaFile;
+  Future<_MediaType> futureMediaType;
+
   @override
-  void initState() {
-    super.initState();
-    futureMediaFile = widget.gallery.loadMedia(widget.media.id);
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final gallery = context.read<GalleryModel>();
+    futureMediaFile = gallery.loadMedia(gallery.openedMedia.id);
     futureMediaType = futureMediaFile.then(_getMediaType);
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: futureMediaFile,
-      builder: (context, AsyncSnapshot<File> snapshot) {
-        if (snapshot.hasError) {
-          if (snapshot.error is CancelledException) {
-            return SizedBox.shrink();
-          } else {
-            throw StateError('Failed to load media: ${snapshot.error}');
-          }
-        }
+    final gallery = context.watch<GalleryModel>();
 
-        if (snapshot.hasData) {
-          return _viewerPage(snapshot.data);
-        } else {
-          return _loadingPage(context);
-        }
-      },
-    );
-  }
-
-  Widget _viewerPage(File mediaFile) {
-    return FutureBuilder(
-      future: futureMediaType,
-      builder: (_, AsyncSnapshot<_MediaType> snapshot) {
-        if (snapshot.hasError) {
-          throw StateError('Failed to get media type: ${snapshot.error}');
-        }
-
-        if (!snapshot.hasData) {
-          return SizedBox.shrink();
-        }
-
-        switch (snapshot.data) {
-          case _MediaType.image:
-            return ImageViewerPage(widget.media, mediaFile);
-          case _MediaType.video:
-            return VideoViewerPage(widget.media, mediaFile);
-          default:
-            throw StateError('Unhandled media type: ${snapshot.data}');
-        }
-      },
-    );
-  }
-
-  Widget _loadingPage(BuildContext context) {
     return Scaffold(
-      appBar: _loadingPageAppBar(),
-      body: _loadingPageBody(context),
+      appBar: AppBar(title: Text(gallery.openedMedia.name)),
+      body: FutureBuilder(
+        future: futureMediaFile,
+        builder: (context, AsyncSnapshot<File> snapshot) {
+          if (snapshot.hasError) {
+            throw snapshot.error;
+          }
+
+          if (snapshot.hasData) {
+            return _mediaView(snapshot.data);
+          } else {
+            return _loadingMediaView(context);
+          }
+        },
+      ),
     );
   }
 
-  AppBar _loadingPageAppBar() {
-    return AppBar(
-      title: Text(widget.media.name),
-      centerTitle: true,
-      automaticallyImplyLeading: true,
-    );
-  }
-
-  Widget _loadingPageBody(BuildContext context) {
+  Widget _loadingMediaView(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
 
     return Center(
       child: Column(
         children: [
           CircularProgressIndicator(),
-          SizedBox(height: 8),
-          Text('Decrypting media', style: textTheme.subtitle2),
+          Text('Decrypting media', style: textTheme.subtitle1),
         ],
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.center,
       ),
     );
   }
+
+  Widget _mediaView(File mediaFile) {
+    return FutureBuilder(
+      future: futureMediaType,
+      builder: (context, AsyncSnapshot<_MediaType> snapshot) {
+        if (snapshot.hasError) {
+          throw snapshot.error;
+        }
+
+        if (!snapshot.hasData) {
+          return SizedBox.expand();
+        }
+
+        switch (snapshot.data) {
+          case _MediaType.image:
+            return ImageView(image: mediaFile);
+          case _MediaType.video:
+            return VideoView(video: mediaFile);
+          case _MediaType.invalid:
+            return _invalidMediaView(context);
+          default:
+            throw StateError('Unhandled media type ${snapshot.data}');
+        }
+      },
+    );
+  }
+
+  Widget _invalidMediaView(BuildContext context) {
+    final gallery = context.watch<GalleryModel>();
+    final textTheme = Theme.of(context).textTheme;
+
+    return Center(
+      child: Padding(
+        child: Text(
+          "'${gallery.openedMedia.name}' is an invalid media file.",
+          style: textTheme.subtitle1,
+        ),
+        padding: EdgeInsets.all(16),
+      ),
+    );
+  }
+}
+
+enum _MediaType { image, video, invalid }
+
+Future<_MediaType> _getMediaType(File media) async {
+  final header = await _readMediaHeader(media);
+  final mime = lookupMimeType(media.path, headerBytes: header);
+
+  if (mime.startsWith('image')) {
+    return _MediaType.image;
+  } else if (mime.startsWith('video')) {
+    return _MediaType.video;
+  } else {
+    return _MediaType.invalid;
+  }
+}
+
+Future<List<int>> _readMediaHeader(File media) async {
+  final chunks = await media.openRead(0, defaultMagicNumbersMaxLength).toList();
+  final header = chunks.expand((c) => c).toList();
+  return header;
 }
